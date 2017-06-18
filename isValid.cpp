c55f798a1c2e59cb04cbcad7cb3b9af29c9699cf
Jorge Manzo <manzoj@oregonstate.edu>
2017-06-17 20:51:36 -0700

    Added Library files
    These files can be used to do a few things in a C string
    -Is the C string an int in its entirety (isValidint())
    -Is the C string an alphabetic string in its entirety(isValidalpha())
    -Is the C string an alphanumeric string in its entirety(isValidalphaInt())
    -Get the size of a C string (getSize())

---
+++ isValid.cpp
@@ -0,0 +1,72 @@
+#include "isValid.h"
+using namespace std;
+int getSize(char *txt){
+    int size =0;
+    for(int i = 0; i < 256; i++){
+        if(txt[i] != '\0'){size++;}
+        else{break;}
+    }
+    return size;
+}
+
+int getSize(const char *txt){
+    int size =0;
+    for(int i = 0; i < 256; i++){
+        if(txt[i] != '\0'){size++;}
+        else{break;}
+    }
+    return size;
+}
+
+bool isValidint(char *txt, int size){
+    for(int i =0; i < size; i++){
+        if( (int)txt[i] >= 48 && (int)txt[i] <= 57){}
+        else{return false;}
+    }
+    return true;
+}
+
+bool isValidalpha(char *txt, int size){
+    for(int i =0; i < size; i++){
+        if( ((int)txt[i] >= 65 && (int)txt[i] <= 90) ||/*check if char is Uppercase*/
+                ((int)txt[i] >= 97 && (int)txt[i] <= 122) /*Checks if char is lowercase*/
+          ){}
+        else{return false;}
+    }
+    return true;
+}
+
+bool isValidalphaInt(char *txt, int size){
+    for(int i =0; i < size; i++){
+        if( 
+            (
+                ((int)txt[i] >= 65 && (int)txt[i] <= 90) /*char is Uppercase Lettter*/||
+                ((int)txt[i] >= 97 && (int)txt[i] <= 122) /*char is lowercase letter*/
+            ) || /*or*/
+            (
+                ((int)txt[i] >= 48 && (int)txt[i] <= 57) /*char is a number 0-9*/
+            )
+          ){}
+        else{return false;}
+    }
+    return true;
+}
+bool isValidalphaInt(char *txt){
+    int size;
+    size = getSize(txt);
+    return isValidalphaInt(txt, size);
+}
+
+bool isValidalpha(char *txt){
+    int size;
+    size = getSize(txt);
+    return isValidalpha(txt, size);
+}
+
+bool isValidint(char *txt){
+    int size;
+    size = getSize(txt);
+    return isValidint(txt, size);
+}
+
+
